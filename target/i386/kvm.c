84fd4b5c090d8be034432b2eecb5eae2f9c1375e
DoomFish <30623715+DoomFishWasTaken@users.noreply.github.com>
2020-08-11 09:20:04 +0300

    Update kvm.c

--- target/i386/kvm.c
+++ target/i386/kvm.c
@@ -1517,7 +1517,7 @@ int kvm_arch_init_vcpu(CPUState *cs)
     }
 
     if (cpu->expose_kvm) {
-        memcpy(signature, "REALCPU", 12);
+        memcpy(signature, "REALCPU", 8);
         c = &cpuid_data.entries[cpuid_i++];
         c->function = KVM_CPUID_SIGNATURE | kvm_base;
         c->eax = KVM_CPUID_FEATURES | kvm_base;
